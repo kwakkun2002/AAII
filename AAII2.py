983392bdb75d1615c939732decf1a04be1b872e4
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2018-09-10 17:59:00 +0900

    9/10 동아리에서:로지스틱회귀에 대해서
    오늘은 로지스틱 회귀에 대해서 배웠다.
    log()에 대해서도 고동현 형이 알으켜주었다.하지만 아직 직관적으로 선형회귀에서 했던 경사하강법과의 연관성을 찾기가 힘들다.다시 한번 복습을 해보아야할것이다.

--- AAII2.py
+++ AAII2.py
@@ -1,68 +1,121 @@
-# import tensorflow as tf
-# import os
+# # import tensorflow as tf
+# # import os
+# #
+# # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# #
+# # x_data = [1,2,3]
+# # y_data = [2,4,6]
+# #
+# # X = tf.placeholder(tf.float32)
+# # Y = tf.placeholder(tf.float32)
+# # W = tf.Variable(tf.random_normal([1], -1, 1))
+# # b = tf.Variable(tf.random_normal([1], -1, 1))
+# #
+# # hypothesis = W * X + b
+# # cost = tf.reduce_mean(tf.square(hypothesis - Y))
+# #
+# # optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
+# #
+# # with tf.Session() as sess:
+# #     sess.run(tf.global_variables_initializer())
+# #
+# #     for i in range(1000):
+# #         _, cost_, val,b_= sess.run([optimizer, cost, W,b], feed_dict={X: x_data, Y: y_data})
+# #         if(i%100 is 0):
+# #             print("try %d cost %f W=%f b=%f" % (i, cost_, val,b_))
 #
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 #
-# x_data = [1,2,3]
-# y_data = [2,4,6]
+# # import tensorflow as tf
+# # import numpy as np
+# #
+# # import os
+# #
+# # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# #
+# # # x,y의 데이터 값
+# # data = [[2, 0], [4, 0], [6, 0], [8, 1], [10, 1], [12, 1], [14, 1]]
+# # x_data = [x_row[0] for x_row in data]
+# # y_data = [y_row[1] for y_row in data]
+# #
+# # # a와 b의 값을 임의로 정함
+# # a = tf.Variable(tf.random_normal([1], dtype=tf.float64, seed=0))
+# # b = tf.Variable(tf.random_normal([1], dtype=tf.float64, seed=0))
+# #
+# # # y 시그모이드 함수의 방정식을 세움
+# # y = 1 / (1 + np.e ** (a * x_data + b))
+# #
+# # # loss를 구하는 함수
+# # loss = -tf.reduce_mean(np.array(y_data) * tf.log(y) + (1 - np.array(y_data)) * tf.log(1 - y))
+# #
+# # # 학습률 값
+# # learning_rate = 0.5
+# #
+# # # loss를 최소로 하는 값 찾기
+# # gradient_decent = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+# #
+# # # 학습
+# # with tf.Session() as sess:
+# #     sess.run(tf.global_variables_initializer())
+# #
+# #     for i in range(60001):
+# #         sess.run(gradient_decent)
+# #         if i % 6000 == 0:
+# #             print("Epoch: %.f, loss = %.4f, 기울기 a = %.4f, 바이어스 b = %.4f" % (i, sess.run(loss), sess.run(a), sess.run(b)))
+# #
+# # tf.sigmoid
 #
-# X = tf.placeholder(tf.float32)
-# Y = tf.placeholder(tf.float32)
-# W = tf.Variable(tf.random_normal([1], -1, 1))
-# b = tf.Variable(tf.random_normal([1], -1, 1))
+# # import tensorflow as tf
+# # import os
+# #
+# # os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+# #
+# # x_data=[[1,2],[2,4],[3,5],[4,1]]
+# # y_data=[[7],[14],[19],[14]]
+# #
+# # X=tf.placeholder(tf.float32,shape=[None,2],name='X')
+# # Y=tf.placeholder(tf.float32,shape=[None,1],name='Y')
+# #
+# # W=tf.Variable(tf.random_normal([2,1],-5,5))
+# # b=tf.Variable(tf.random_normal([1],-5,5))
+# #
+# # Hypoth=tf.matmul(X,W)+b
+# # cost=tf.reduce_mean(tf.square(Y-Hypoth))
+# # optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
+# #
+# #
+# # with tf.Session() as sess:
+# #     sess.run(tf.global_variables_initializer())
+# #
+# #     for i in range(30000):
+# #         sess.run(optimizer,feed_dict={X:x_data,Y:y_data})
+# #
+# #         if(i%100 is 0):
+# #             print(i,sess.run(W),'\tb:',sess.run(b))
 #
-# hypothesis = W * X + b
-# cost = tf.reduce_mean(tf.square(hypothesis - Y))
-#
-# optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
-#
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#
-#     for i in range(1000):
-#         _, cost_, val,b_= sess.run([optimizer, cost, W,b], feed_dict={X: x_data, Y: y_data})
-#         if(i%100 is 0):
-#             print("try %d cost %f W=%f b=%f" % (i, cost_, val,b_))
+import tensorflow as tf
+import os
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
-# import tensorflow as tf
-# import numpy as np
-#
-# import os
-#
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-#
-# # x,y의 데이터 값
-# data = [[2, 0], [4, 0], [6, 0], [8, 1], [10, 1], [12, 1], [14, 1]]
-# x_data = [x_row[0] for x_row in data]
-# y_data = [y_row[1] for y_row in data]
-#
-# # a와 b의 값을 임의로 정함
-# a = tf.Variable(tf.random_normal([1], dtype=tf.float64, seed=0))
-# b = tf.Variable(tf.random_normal([1], dtype=tf.float64, seed=0))
-#
-# # y 시그모이드 함수의 방정식을 세움
-# y = 1 / (1 + np.e ** (a * x_data + b))
-#
-# # loss를 구하는 함수
-# loss = -tf.reduce_mean(np.array(y_data) * tf.log(y) + (1 - np.array(y_data)) * tf.log(1 - y))
-#
-# # 학습률 값
-# learning_rate = 0.5
-#
-# # loss를 최소로 하는 값 찾기
-# gradient_decent = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
-#
-# # 학습
-# with tf.Session() as sess:
-#     sess.run(tf.global_variables_initializer())
-#
-#     for i in range(60001):
-#         sess.run(gradient_decent)
-#         if i % 6000 == 0:
-#             print("Epoch: %.f, loss = %.4f, 기울기 a = %.4f, 바이어스 b = %.4f" % (i, sess.run(loss), sess.run(a), sess.run(b)))
-#
-# tf.sigmoid
+x_data=[1,2,3]
+y_data=[0,1,1]
 
-a=[int(i) for i in input().split()]
-print(a)
+X=tf.placeholder(tf.float32,shape=[None],name='X')
+Y=tf.placeholder(tf.float32,shape=[None],name='Y')
+
+W=tf.Variable(tf.random_normal([1],-5,5))
+b=tf.Variable(tf.random_normal([1],-5,5))
+
+hypoth=tf.sigmoid(W*X+b)
+cost=-tf.reduce_mean(Y*tf.log(hypoth)+(1-Y)*tf.log(1-hypoth))
+optimizer=tf.train.GradientDescentOptimizer(0.1).minimize(cost)
+
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    for i in range(10000):
+        sess.run(optimizer,feed_dict={X:x_data,Y:y_data})
+        if(i%100 is 0):
+            print(sess.run(W),sess.run(b))
+    predicted=tf.cast(hypoth>0.5,dtype=tf.float32)
+    print(sess.run(predicted,feed_dict={X:x_data}))
+
