bcd47e14c687677699bc5d9cca57787bc7b18641
kwakkun2002 <36096367+kwakkun2002@users.noreply.github.com>
2018-09-22 20:34:10 +0900

    9/22 숫자맞추기게임
    선형회귀를 통해서 숫자 맞추기 게임을
    해보았다.약간의 오차가 있지만 유사한 정답이 나왔다

--- AALL3.py
+++ AALL3.py
@@ -108,42 +108,198 @@
 #     print(cost_)
 
 # 9/17 mnist
-import tensorflow as tf
-from tensorflow.examples.tutorials.mnist import input_data
+# import tensorflow as tf  # 평화로운 텐서플로우 임포트
+# from tensorflow.examples.tutorials.mnist import input_data  # mnist가져오기
+#
+# mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)  # 원_핫을 키고?mnist에 데이터저장
+#
+# X = tf.placeholder(tf.float32, shape=[None, 784], name='X')  # X는 한줄에 784짜리 행렬 행의 개수 미정
+# Y = tf.placeholder(tf.float32, shape=[None, 10], name='Y')  # Y는 0-9까지의 숫자이기 때문에 one_hot으로 10개
+# # 원_핫이 약간 omr같은건가(요)?아직 softmax가 완벽하게이해...ㅠㅠ
+#
+# W = tf.Variable(tf.random_normal([784, 10]))  # 적절한 행렬곱으로 적절하게 구할수 있음
+# b = tf.Variable(tf.random_normal([10]))  # 이것도 행렬로 알수있음
+#
+# Hy = tf.nn.softmax(tf.matmul(X, W) + b)  # 소프트맥스 가설
+# cost = -tf.reduce_mean(tf.reduce_sum(Y * tf.log(Hy)))  # 코스트
+# optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(cost)  # 코스트 최소화
+#
+# is_correct = tf.equal(tf.arg_max(Hy, 1), tf.arg_max(Y, 1))  # 근데 왜 1이 들어가죠?저 max가 최대값찾는
+# accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))  # 고동형이 cast는 map이랑 비슷하댔어!!아마 is_correct는 0이나 1이 오겠지
+#
+# total_epoch = 15  # 반복횟수
+# batch_size = 100  # 자를 크기가 아닌 자를 횟수라고 해야하나
+# total_batch = int(mnist.train.num_examples / batch_size)  # 이게 총 개수
+# # 그러니까 데이터가 크니까 자른거임 근데 너무 자르면 학습이 이상하게 된다 그래서 적당히 자르는게 중요!
+#
+# with tf.Session() as sess:  # cpu가 신나는 세션열기
+#     sess.run(tf.global_variables_initializer())  # 변수 초기화
+#
+#     for epoch in range(total_epoch):  # 반복하자 근데 굳이 15인 이유는??
+#         sum_cost = 0  # 코스트를 계속 누적시키겠지...
+#
+#         for i in range(total_batch):  # 자른만큼 돌자
+#             batch_x, batch_y = mnist.train.next_batch(batch_size)  # 값을 신기하게 받아옴;
+#
+#             _, cost_ = sess.run([optimizer, cost], feed_dict={X: batch_x, Y: batch_y})  # _는 안쓰고,cost_는 코스트
+#
+#             sum_cost += cost_  # 누적누적
+#
+#         print('epoch:%4d,cost:%9f' % (epoch + 1, sum_cost))  # 횟수와 코스트 출력
+#
+#     print("accuracy:",
+#           sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))  # 얼마나 학습이 잘됬는지 보는 accuarcy
+#     print("learning end")  # 이야 끝났다!!복습ㄱㄱ나머지도 주석ㄱㄱ
+#     # 항상 느끼는건데 결국 cost랑 hy랑 accuracy만 바뀌는것 같은데...
 
-mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
+# 9/19
+# import tensorflow as tf
+# # 역사 다층퍼셉트론의 등장l
+# import numpy as np
+#
+# tf.set_random_seed(777)  # for reproducibility
+# learning_rate = 0.1
+#
+# x_data = [[0, 0],
+#           [0, 1],
+#           [1, 0],
+#           [1, 1]]
+# y_data = [[0],
+#           [1],
+#           [1],
+#           [0]]
+# x_data = np.array(x_data, dtype=np.float32)
+# y_data = np.array(y_data, dtype=np.float32)
+#
+# X = tf.placeholder(tf.float32, [None, 2])
+# Y = tf.placeholder(tf.float32, [None, 1])
+#
+# W1 = tf.Variable(tf.random_normal([2, 2]), name='weight1')
+# b1 = tf.Variable(tf.random_normal([2]), name='bias1')
+# layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
+#
+# W2 = tf.Variable(tf.random_normal([2, 1]), name='weight2')
+# b2 = tf.Variable(tf.random_normal([1]), name='bias2')
+# hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
+#
+# # cost/loss function
+# cost = -tf.reduce_mean(Y * tf.log(hypothesis) + (1 - Y) *
+#                        tf.log(1 - hypothesis))
+#
+# train = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
+#
+# # Accuracy computation
+# # True if hypothesis>0.5 else False
+# predicted = tf.cast(hypothesis > 0.5, dtype=tf.float32)
+# accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
+#
+# # Launch graph
+# with tf.Session() as sess:
+#     # Initialize TensorFlow variables
+#     sess.run(tf.global_variables_initializer())
+#
+#     for step in range(10001):
+#         sess.run(train, feed_dict={X: x_data, Y: y_data})
+#         if step % 100 == 0:
+#             print(step, sess.run(cost, feed_dict={
+#                   X: x_data, Y: y_data}), sess.run([W1, W2]))
+#
+#     # Accuracy report
+#     h, c, a = sess.run([hypothesis, predicted, accuracy],
+#                        feed_dict={X: x_data, Y: y_data})
+#     print("\nHypothesis: ", h, "\nCorrect: ", c, "\nAccuracy: ", a)
 
-X = tf.placeholder(tf.float32, shape=[None, 784], name='X')
-Y = tf.placeholder(tf.float32, shape=[None, 10], name='Y')
+# 9/21 다층 퍼셉트론 xor
+# import tensorflow as tf
+# import os
+#
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#
+# x_data = [
+#     [0, 0],
+#     [0, 1],
+#     [1, 0],
+#     [1, 1]
+# ]
+# y_data = [
+#     [0],
+#     [1],
+#     [1],
+#     [0]
+# ]
+#
+# X = tf.placeholder(tf.float32, shape=[None, 2])
+# Y = tf.placeholder(tf.float32, shape=[None, 1])
+#
+# W1 = tf.Variable(tf.random_normal([2, 100]), name='W1')
+# b1 = tf.Variable(tf.random_normal([100]), name='b1')
+# h1 = tf.nn.sigmoid(tf.add(tf.matmul(X, W1), b1))
+#
+# W2 = tf.Variable(tf.random_normal([100, 100]), name='W2')
+# b2 = tf.Variable(tf.random_normal([100]), name='b2')
+# h2 = tf.nn.sigmoid(tf.add(tf.matmul(h1, W2), b2))
+#
+# W3 = tf.Variable(tf.random_normal([100, 100]), name='W3')
+# b3 = tf.Variable(tf.random_normal([100]), name='b3')
+# h3 = tf.nn.sigmoid(tf.add(tf.matmul(h2, W3), b3))
+#
+# W4 = tf.Variable(tf.random_normal([100, 1]), name='W4')
+# b4 = tf.Variable(tf.random_normal([1]), name='b4')
+# h4 = tf.nn.sigmoid(tf.add(tf.matmul(h3, W4), b4))
+#
+# cost = -tf.reduce_mean(Y * tf.log(h4) + (1 - Y) * tf.log(1 - h4))
+# optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(cost)
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     for i in range(100000):
+#         sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
+#         cost_ = sess.run(cost, feed_dict={X: x_data, Y: y_data})
+#
+#         if (i % 100 is 0):
+#             print(sess.run([W1, W2]))
+#             print(cost_)
+#             print()
+#
+#     print(sess.run(h4, feed_dict={X: x_data}))
 
-W = tf.Variable(tf.random_normal([784, 10]))
-b = tf.Variable(tf.random_normal([10]))
+#9/22 숫자 맞추기 게임
+# import tensorflow as tf
+# import os
+#
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+#
+# x_data = [
+#     [1, 2],
+#     [3, 4],
+#     [5, 6],
+#     [7, 8],
+#     [9, 10],
+#     [10, 11]
+# ]
+# y_data = [
+#     [2],
+#     [12],
+#     [30],
+#     [56],
+#     [90],
+#     [110]
+# ]
+#
+# X = tf.placeholder(tf.float32, shape=[None,2], name='X')
+# Y = tf.placeholder(tf.float32, shape=[None,1], name='Y')
+#
+# W = tf.Variable(tf.random_normal([2,1]))
+# b = tf.Variable(tf.random_normal([1]))
+#
+# Hy = tf.add(tf.matmul(X,W),b)
+# cost = tf.reduce_mean(tf.square(Y - Hy))
+# optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
+#
+# with tf.Session() as sess:
+#     sess.run(tf.global_variables_initializer())
+#     for i in range(10000):
+#         sess.run(optimizer, feed_dict={X: x_data, Y: y_data})
+#     answer=sess.run(Hy,feed_dict={X:[[20,20]]})
+#     print(int(answer))
 
-Hy = tf.nn.softmax(tf.matmul(X, W) + b)
-cost = -tf.reduce_mean(tf.reduce_sum(Y * tf.log(Hy)))
-optimizer = tf.train.GradientDescentOptimizer(0.001).minimize(cost)
-
-is_correct = tf.equal(tf.arg_max(Hy, 1), tf.arg_max(Y, 1))
-accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
-
-total_epoch = 15
-batch_size = 100
-total_batch = int(mnist.train.num_examples / batch_size)
-
-with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer())
-
-    for epoch in range(total_epoch):
-        sum_cost = 0
-
-        for i in range(total_batch):
-            batch_x, batch_y = mnist.train.next_batch(batch_size)
-
-            _, cost_ = sess.run([optimizer, cost], feed_dict={X: batch_x, Y: batch_y})
-
-            sum_cost += cost_
-
-        print('epoch:%4d,cost:%9f' % (epoch + 1, sum_cost))
-
-    print("accuracy:", sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels}))
-    print("learning end")
